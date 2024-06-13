b3bb26f2f29b31cdd400684a0bdb39bac3f8b0c0
Rajat Sharda <rajatsharda@Rajats-MacBook-Air.local>
2024-06-13 01:38:34 -0400

    initial workings

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('Hello World')
+import os
+import google.generativeai as genai
+from dotenv import load_dotenv
+import PIL.Image
+load_dotenv()
+
+api_key = os.environ["GEMINI_API_KEY"]
+
+genai.configure(api_key=api_key)
+
+model = genai.GenerativeModel('gemini-1.5-flash')
+
+img = PIL.Image.open('assets/img7.png')
+
+response = model.generate_content(["Write a one line generic description of the image.", img], stream=True)
+response.resolve()
+print(response.text)
